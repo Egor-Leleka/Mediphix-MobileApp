05e7d0a2190b5bd1f853dd8eee351dc6b2d0eb87
harshdalal0077 <harshdalal0077@gmail.com>
2023-11-07 21:58:14 +1300

    Checks Data Model class Created.

---
+++ app/src/main/java/com/example/mediphix_app/Checks.kt
@@ -0,0 +1,7 @@
+package com.example.mediphix_app
+
+data class Checks(val regNumber : String? = null,
+                  val Nurse_name : String? = null,
+                  val Storage_Location : String? = null,
+                  val drugList : String? = null,
+                  val checkDate : String? = null,){}
